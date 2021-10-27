5dcaafcd2032b3a1d0f854fc8ba6f7c9f1731cf5
Vera <volkovat231@gmail.com>
2021-10-27 11:45:13 +0300

    полезный код1

--- newProject/newProject/Program.cs
+++ newProject/newProject/Program.cs
@@ -6,7 +6,7 @@ namespace newProject
     {
         static void Main(string[] args)
         {
-            int a = 5;
+            int a = 52;
             int b = 10;
             int c = a + b;
             Console.WriteLine(c);
