33734f29dd5c75ea3e953398183a1cd6ccd105d2
Vera <volkovat231@gmail.com>
2021-10-27 12:31:56 +0300

    sasdd

--- newProject/newProject/Program.cs
+++ newProject/newProject/Program.cs
@@ -6,7 +6,7 @@ namespace newProject
     {
         static void Main(string[] args)
         {
-            int a = 19;
+            int a = 12;
             int b = 10;
             int c = a + b;
             Console.WriteLine(c);
